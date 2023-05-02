999f33bb54457e238cb4555737cb112d89abd9e7
black-wyvern-dev <tuktarov2121@gmail.com>
2023-05-02 20:38:24 +0900

    fix: mobile reponsive

--- src/components/SpaceKittyClaimpage.tsx
+++ src/components/SpaceKittyClaimpage.tsx
@@ -74,17 +74,17 @@ export default function SpaceKittyClaimpage(props: {
   };
 
   return (
-    <div className="flex flex-col gap-4 bg-white rounded-lg min-h-[40vh]">
+    <div className="flex flex-col gap-4 bg-white rounded-lg min-h-[45vh]">
       <div className="px-4 -mt-6">
         <div className="w-full py-4 text-center text-white rounded-lg box-shadow-custom">
           <h1 className="text-3xl text-white uppercase">spacekitty nfts</h1>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center w-full gap-5">
-        <h1 className="text-2xl font-bold text-gray-700 uppercase">
+        <h1 className="text-xl font-bold text-gray-700 uppercase">
           Total Nft Counts : {props.nftCounts}
         </h1>
-        <h1 className="text-2xl font-bold text-gray-700 uppercase">
+        <h1 className="text-xl font-bold text-gray-700 uppercase">
           claim amount : {props.claimAmount}
         </h1>
       </div>
